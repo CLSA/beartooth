209e7398060c3f5abfb9d7209d240f97174c38ac
Patrick Emond <emondpd@mcmaster.ca>
2011-10-05 17:24:54 -0400

    Modifying for home/site interviewing purposes [WIP]

--- sql/schema.sql
+++ sql/schema.sql
@@ -676,29 +676,15 @@ CREATE  TABLE IF NOT EXISTS `appointment` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `update_timestamp` TIMESTAMP NOT NULL ,
   `create_timestamp` TIMESTAMP NOT NULL ,
-  `participant_id` INT UNSIGNED NOT NULL ,
-  `phone_id` INT UNSIGNED NULL DEFAULT NULL ,
-  `assignment_id` INT UNSIGNED NULL DEFAULT NULL COMMENT 'This appointment\'s assignment.' ,
+  `address_id` INT UNSIGNED NOT NULL ,
   `datetime` DATETIME NOT NULL ,
   `reached` TINYINT(1)  NULL DEFAULT NULL COMMENT 'If the appointment was met, whether the participant was reached.' ,
   PRIMARY KEY (`id`) ,
-  INDEX `fk_participant_id` (`participant_id` ASC) ,
-  INDEX `fk_assignment_id` (`assignment_id` ASC) ,
   INDEX `dk_reached` (`reached` ASC) ,
-  INDEX `fk_phone_id` (`phone_id` ASC) ,
-  CONSTRAINT `fk_appointment_participant`
-    FOREIGN KEY (`participant_id` )
-    REFERENCES `participant` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_appointment_assignment`
-    FOREIGN KEY (`assignment_id` )
-    REFERENCES `assignment` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_appointment_phone_id`
-    FOREIGN KEY (`phone_id` )
-    REFERENCES `phone` (`id` )
+  INDEX `fk_address_id` (`address_id` ASC) ,
+  CONSTRAINT `fk_appointment_address_id`
+    FOREIGN KEY (`address_id` )
+    REFERENCES `address` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
