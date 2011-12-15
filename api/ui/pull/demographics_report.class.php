ad3bca89e2c59e2e7680487ef784f014bd26f006
Patrick Emond <emondpd@mcmaster.ca>
2011-12-15 15:06:07 -0500

    Fixing bug in reports caused by participants with no address

--- api/ui/pull/demographics_report.class.php
+++ api/ui/pull/demographics_report.class.php
@@ -52,8 +52,11 @@ class demographics_report extends \cenozo\ui\pull\base_report
       $db_consent = $db_participant->get_last_consent();
       if( is_null( $db_consent ) && $consent_status != 'Any' ) continue;
       
-      $region_id = $db_participant->get_primary_address()->get_region()->id;
-      $region_name = $db_participant->get_primary_address()->get_region()->name;
+      $db_address = $db_participant->get_primary_address();
+      if( is_null( $db_address ) ) continue;
+
+      $region_id = $db_address->get_region()->id;
+      $region_name = $db_address->get_region()->name;
 
       if( ( 'deceased' == $db_participant->status ) ||   
           ( $province_id && $province_id != $region_id ) ||
