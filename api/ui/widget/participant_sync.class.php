4992df5791557af464fe23c5d1d9e1b5a45db37c
Patrick Emond <emondpd@mcmaster.ca>
2011-12-15 15:32:16 -0500

    Adding missing participan_sync files

---
+++ api/ui/widget/participant_sync.class.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * participant_sync.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @package beartooth\ui
+ * @filesource
+ */
+
+namespace beartooth\ui\widget;
+use cenozo\lib, cenozo\log;
+
+/**
+ * widget participant sync
+ * 
+ * @package beartooth\ui
+ */
+class participant_sync extends \cenozo\ui\widget
+{
+  /**
+   * Constructor
+   * 
+   * Defines all variables which need to be set for the associated template.
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param array $args An associative array of arguments to be processed by the widget
+   * @access public
+   */
+  public function __construct( $args )
+  {
+    parent::__construct( 'participant', 'sync', $args );
+  }
+
+  /**
+   * Finish setting the variables in a widget.
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @access public
+   */
+  public function finish()
+  {
+    parent::finish();
+  }
+}
+?>
