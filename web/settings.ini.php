bee82e8657f1e98300e8540e4a04e525dec36955
Patrick Emond <emondpd@mcmaster.ca>
2012-01-30 15:50:26 -0500

    Fixing version number (this is 0.1.1)

--- web/settings.ini.php
+++ web/settings.ini.php
@@ -11,7 +11,7 @@ global $SETTINGS;
 
 // tagged version
 $SETTINGS['general']['application_name'] = 'beartooth';
-$SETTINGS['general']['version'] = '0.1.0';
+$SETTINGS['general']['version'] = '0.1.1';
 
 // always leave as false when running as production server
 $SETTINGS['general']['development_mode'] = false;
