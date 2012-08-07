42572c24a2eeff541c3e650768da3cf69ffa6c1f
Patrick Emond <emondpd@mcmaster.ca>
2012-08-07 10:47:41 -0400

    Adding a default to the qnaire_id parameter in the queue_view widget

--- api/ui/widget/queue_view.class.php
+++ api/ui/widget/queue_view.class.php
@@ -52,7 +52,7 @@ class queue_view extends \cenozo\ui\widget\base_view
       if( !is_null( $site_id ) ) $this->db_site = lib::create( 'database\site', $site_id );
     }
 
-    $qnaire_id = $this->get_argument( 'qnaire_id' );
+    $qnaire_id = $this->get_argument( 'qnaire_id', 0 );
     if( $qnaire_id ) $this->db_qnaire = lib::create( 'database\qnaire', $qnaire_id );
 
     $this->language = $this->get_argument( 'language' );
