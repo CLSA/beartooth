e7006322794073933dfb35bd7dd20b66a6ddd135
Patrick Emond <emondpd@mcmaster.ca>
2012-08-17 14:42:39 -0400

    Adding functionality to participant list for Mastodon's quota report
    This update includes replacing the participant_phone_call_status_count view
    with a more valid interview_phone_call_status_count view and adding the new
    participant_last_appointment and participant_last_consent views.

--- sql/upgrade/1.0.4/patch_database.sql
+++ sql/upgrade/1.0.4/patch_database.sql
@@ -2,3 +2,7 @@
 
 SOURCE participant_last_assignment.sql
 SOURCE interview_last_assignment.sql
+SOURCE participant_phone_call_status_count.sql
+SOURCE interview_phone_call_status_count.sql
+SOURCE participant_last_appointment.sql
+SOURCE participant_last_consent.sql
