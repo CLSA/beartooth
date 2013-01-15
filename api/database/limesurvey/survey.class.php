ef00e98c518fb6ab03bf7e2fa5212d5cca772407
Patrick Emond <emondpd@mcmaster.ca>
2013-01-15 17:59:05 -0500

    Repairing and consolodating limesurvey db layer

--- api/database/limesurvey/survey.class.php
+++ api/database/limesurvey/survey.class.php
@@ -14,11 +14,19 @@ use cenozo\lib, cenozo\log, beartooth\util;
  */
 class survey extends sid_record
 {
+  /**
+   * Returns a participant's response to a particular question.
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param string $question_code
+   * @return string
+   * @access public
+   */
   public function get_response( $question_code )
   {
     // the questions table has more than one column in its primary key so custom sql is needed
     $modifier = lib::create( 'database\modifier' );
-    $modifier->where( 'sid', '=', static::$table_sid );
+    $modifier->where( 'sid', '=', static::get_sid() );
     $modifier->where( 'title', '=', $question_code );
     $modifier->group( 'sid' );
     $modifier->group( 'gid' );
@@ -28,8 +36,32 @@ class survey extends sid_record
                     $modifier->get_sql() );
     
     $row = static::db()->get_row( $sql );
-    $column_name = sprintf( '%sX%sX%s', static::$table_sid, $row['gid'], $row['qid'] );
+    if( 0 == count( $row ) )
+      throw lib::create( 'exception\runtime', 'Question code not found in survey.', __METHOD__ );
+
+    $column_name = sprintf( '%sX%sX%s', static::get_sid(), $row['gid'], $row['qid'] );
     return $this->$column_name;
+  }
+
+  /**
+   * Returns the total time in seconds spent on this survey (by all participants)
+   * 
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @param database\modifier $modifier
+   * @return double
+   * @static
+   */
+  public static function get_total_time( $modifier = NULL )
+  {
+    $table_name = static::get_table_name();
+    if( is_null( $modifier ) ) $modifier = lib::create( 'database\modifier' );
+    $modifier->where( $table_name.'.id', '=', $table_name.'_timings.id', false );
+
+    return static::db()->get_one( sprintf(
+      'SELECT SUM( IFNULL( interviewtime, 0 ) ) FROM %s, %s %s',
+      $table_name,
+      $table_name.'_timings',
+      $modifier->get_sql() ) );
   }
 
   /**
