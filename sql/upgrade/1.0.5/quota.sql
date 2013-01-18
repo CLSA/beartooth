1adb51d2d35f5b013923f09fe4112155411ca536
Patrick Emond <emondpd@mcmaster.ca>
2013-01-18 16:27:01 -0500

    Tweaking sql upgrade scripts

--- sql/upgrade/1.0.5/quota.sql
+++ sql/upgrade/1.0.5/quota.sql
@@ -30,6 +30,23 @@ CREATE PROCEDURE patch_quota()
       ADD UNIQUE INDEX uq_region_id_site_id_gender_age_group_id
       (region_id ASC, site_id ASC, gender ASC, age_group_id ASC);
 
+      UPDATE site SET region_id = ( SELECT id FROM region WHERE abbreviation = "BC")
+      WHERE name = "British Columbia";
+      DELETE FROM quota;
+      ALTER TABLE quota AUTO_INCREMENT = 1;
+      INSERT INTO quota ( region_id, site_id, gender, age_group_id, population )
+      SELECT r.id, s.id, g.gender, a.id, 300
+      FROM region r join site s on r.id = s.region_id,
+           ( select distinct gender from participant ) g,
+           age_group a
+      ORDER BY r.id, s.id, a.id, g.gender;
+      UPDATE quota SET population = 450 WHERE age_group_id IN (
+        SELECT id FROM age_group WHERE lower < 60
+      );
+      UPDATE quota SET population = population / 2 WHERE site_id IN (
+        SELECT id FROM site WHERE name IN ( "Simon Fraser", "British Columbia" )
+      );
+
       SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
       SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
     END IF;
