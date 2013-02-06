a4bcd427d53706ae5e99b3b5da23bc79c2cdfcef
Patrick Emond <emondpd@mcmaster.ca>
2013-02-06 15:50:02 -0500

    Adding site_voip table to extend site record

--- sql/schema.sql
+++ sql/schema.sql
@@ -515,6 +515,25 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `beartooth`.`site_voip`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `beartooth`.`site_voip` ;
+
+CREATE  TABLE IF NOT EXISTS `beartooth`.`site_voip` (
+  `site_id` INT UNSIGNED NOT NULL ,
+  `host` VARCHAR(45) NULL DEFAULT NULL ,
+  `xor_key` VARCHAR(45) NULL DEFAULT NULL ,
+  PRIMARY KEY (`site_id`) ,
+  INDEX `fk_site_id` (`site_id` ASC) ,
+  CONSTRAINT `fk_site_voip_site_id`
+    FOREIGN KEY (`site_id` )
+    REFERENCES `cenozo`.`site` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Placeholder table for view `beartooth`.`assignment_last_phone_call`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `beartooth`.`assignment_last_phone_call` (`assignment_id` INT, `phone_call_id` INT);
