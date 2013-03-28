a90e2d68b48627fde72abc51aa68c7fd1ab8ea12
Patrick Emond <emondpd@mcmaster.ca>
2013-03-28 11:35:02 -0400

    0000432: No Withdraw Cancel Button

--- api/ui/widget/self_assignment.class.php
+++ api/ui/widget/self_assignment.class.php
@@ -126,9 +126,12 @@ class self_assignment extends \cenozo\ui\widget
       sprintf( $db_participant->first_name.' '.$db_participant->last_name ) );
     $this->set_variable( 'participant_uid', $db_participant->uid );
     $this->set_variable( 'participant_language', $language );
-    $this->set_variable( 'participant_consent', $consent );
-    $this->set_variable( 'withdrawing', 'withdraw' == $consent );
-    $this->set_variable( 'allow_withdraw', !is_null( $db_interview->get_qnaire()->withdraw_sid ) );
+    $this->set_variable(
+      'participant_consent', is_null( $db_last_consent ) ? 'none' : $db_last_consent->to_string() );
+    $this->set_variable(
+      'withdrawing', !is_null( $db_last_consent ) && false == $db_last_consent->accept );
+    $this->set_variable(
+      'allow_withdraw', !is_null( $db_interview->get_qnaire()->withdraw_sid ) );
     
     // get the callback associated with this assignment, if any
     $modifier = lib::create( 'database\modifier' );
