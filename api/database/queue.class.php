3e2d3b45cafc710e9c399c81d64b9e5bd7aff5f0
Patrick Emond <emondpd@mcmaster.ca>
2014-02-04 19:03:46 -0500

    Adding missing DISTINCT keywords to select queries in queue record

--- api/database/queue.class.php
+++ api/database/queue.class.php
@@ -306,7 +306,7 @@ class queue extends \cenozo\database\record
       $sql = sprintf(
         'INSERT INTO queue_has_participant( '.
           'participant_id, queue_id, site_id, qnaire_id, start_qnaire_date, address_id ) '.
-        'SELECT queue_has_participant.participant_id, %s, site_id, qnaire_id, start_qnaire_date, '.
+        'SELECT DISTINCT queue_has_participant.participant_id, %s, site_id, qnaire_id, start_qnaire_date, '.
                'queue_has_participant.address_id '.
         'FROM queue_has_participant '.
         'JOIN appointment ON queue_has_participant.participant_id = appointment.participant_id '.
@@ -354,7 +354,7 @@ class queue extends \cenozo\database\record
       $sql = sprintf(
         'INSERT INTO queue_has_participant( '.
           'participant_id, queue_id, site_id, qnaire_id, start_qnaire_date, address_id ) '.
-        'SELECT queue_has_participant.participant_id, %s, site_id, qnaire_id, start_qnaire_date, '.
+        'SELECT DISTINCT queue_has_participant.participant_id, %s, site_id, qnaire_id, start_qnaire_date, '.
                'queue_has_participant.address_id '.
         'FROM queue_has_participant '.
         'JOIN callback ON queue_has_participant.participant_id = callback.participant_id '.
@@ -394,7 +394,7 @@ class queue extends \cenozo\database\record
       $sql = sprintf(
         'INSERT INTO queue_has_participant( '.
           'participant_id, queue_id, site_id, qnaire_id, start_qnaire_date, address_id ) '.
-        'SELECT queue_has_participant.participant_id, %s, site_id, qnaire_id, start_qnaire_date, '.
+        'SELECT DISTINCT queue_has_participant.participant_id, %s, site_id, qnaire_id, start_qnaire_date, '.
                'queue_has_participant.address_id '.
         'FROM queue_has_participant '.
         'JOIN participant_last_interview '.
