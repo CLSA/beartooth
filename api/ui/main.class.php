2fb6a849f6a4c8c1de6cf3f2a6897654227cd83b
Patrick Emond <emondpd@mcmaster.ca>
2015-01-22 13:58:46 -0500

    Adding missing voip code in Beartooth

--- api/ui/main.class.php
+++ api/ui/main.class.php
@@ -24,10 +24,39 @@ class main extends \cenozo\ui\main
   {
     $session = lib::create( 'business\session' );
     $survey_manager = lib::create( 'business\survey_manager' );
+    $setting_manager = lib::create( 'business\setting_manager' );
+    $voip_manager = lib::create( 'business\voip_manager' );
+    $db_site = $session->get_site();
+    
     $variables = parent::get_variables();
     $variables['survey_url'] = $survey_manager->get_survey_url();
     $variables['show_menu'] =
       is_null( $session->get_current_assignment() ) && false == $variables['survey_url'];
+
+    $voip_enabled = $setting_manager->get_setting( 'voip', 'enabled' );
+    
+    // get the xor key and make sure it is at least as long as the password
+    $xor_key = $setting_manager->get_setting( 'voip', 'xor_key' );
+    $password = $_SERVER['PHP_AUTH_PW'];
+
+    // avoid infinite loops by using a counter
+    $counter = 0;
+    while( strlen( $xor_key ) < strlen( $password ) )
+    {
+      $xor_key .= $xor_key;
+      if( 1000 < $counter++ ) break;
+    }
+    
+    $variables['webphone_parameters'] = sprintf(
+      'username=%s&password=%s',
+      $_SERVER['PHP_AUTH_USER'],
+      base64_encode( $password ^ $xor_key ) );
+    $variables['webphone'] = $voip_enabled && !$voip_manager->get_sip_enabled();
+    $variables['dialpad'] = !is_null( $voip_manager->get_call() );
+    $variables['calculator'] = true;
+    $variables['timezone_calculator'] = true;
+    $variables['navigation'] = is_null( $session->get_current_assignment() );
+
     return $variables;
   }
 }
