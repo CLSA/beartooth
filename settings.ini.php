2133fa7d5b3c7bb873ca29ccaad40b70ae800add
Patrick Emond <emondpd@mcmaster.ca>
2016-08-30 11:22:16 -0400

    Was instantiating CnScriptLauncher with the old uid argument
    The script launcher now accepts an identifier instead of a uid argument.
    This has been corrected in the assignment module.

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.0.0';
-$SETTINGS['general']['build'] = 'edcd8d0';
+$SETTINGS['general']['build'] = 'fe80c43';
 
 // always leave as false when running as production server
 $SETTINGS['general']['development_mode'] = false;
