450ec0bdd65ca254d508a2f379e759ce8fee7257
Patrick Emond <emondpd@mcmaster.ca>
2016-10-21 17:26:17 -0400

    Working on participant export

--- db/upgrade/2.0.0/export_column.sql
+++ db/upgrade/2.0.0/export_column.sql
@@ -5,13 +5,14 @@ CREATE TABLE IF NOT EXISTS export_column (
   update_timestamp TIMESTAMP NOT NULL,
   create_timestamp TIMESTAMP NOT NULL,
   export_id INT UNSIGNED NOT NULL,
-  rank INT UNSIGNED NOT NULL,
   table_name VARCHAR(45) NOT NULL,
-  column_name VARCHAR(45) NOT NULL,
   subtype VARCHAR(45) NULL DEFAULT NULL,
+  column_name VARCHAR(45) NOT NULL,
+  rank INT UNSIGNED NOT NULL,
   PRIMARY KEY (id),
   INDEX fk_export_id (export_id ASC),
-  UNIQUE INDEX uq_export_id_rank (export_id ASC, rank ASC),
+  UNIQUE INDEX uq_export_rank (export_id ASC, rank ASC),
+  INDEX dk_table_name_subtype (table_name ASC, subtype ASC),
   CONSTRAINT fk_export_column_export_id
     FOREIGN KEY (export_id)
     REFERENCES export (id)
