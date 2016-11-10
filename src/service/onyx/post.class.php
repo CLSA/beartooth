138ec9ea6425c6a73ff6501b71d89d6533f89b31
Patrick Emond <emondpd@mcmaster.ca>
2016-11-10 15:07:37 -0500

    service\onyx\post: Ignoring manual proxy forms

--- src/service/onyx/post.class.php
+++ src/service/onyx/post.class.php
@@ -430,7 +430,8 @@ class post extends \cenozo\service\service
    */
   private function process_proxy( $db_participant, $object )
   {
-    if( 1 >= count( get_object_vars( $object ) ) ) return;
+    // ignore empty or manual forms
+    if( 1 >= count( get_object_vars( $object ) ) || 'MANUAL' == strtoupper( $object->mode ) ) return;
 
     $region_class_name = lib::get_class_name( 'database\region' );
     $application_class_name = lib::get_class_name( 'database\application' );
