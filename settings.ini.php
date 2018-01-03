561cae9a165d49b85e2193365238f1586b1828be
Patrick Emond <emondpd@mcmaster.ca>
2018-01-03 16:26:08 -0500

    Minifying js and css files

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.2';
-$SETTINGS['general']['build'] = 'c3c868c';
+$SETTINGS['general']['build'] = '2d1576b';
 
 // the location of beartooth internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
