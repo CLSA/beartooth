44d717ebb31f203b287e0f6bb466c6e108984c29
Patrick Emond <emondpd@mcmaster.ca>
2018-12-12 10:46:46 -0500

    Preparing for version 2.4

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.3';
+$SETTINGS['general']['version'] = '2.4';
 $SETTINGS['general']['build'] = 'fe2665e';
 
 // determines whether users other than administrators can see next-of-kin data
