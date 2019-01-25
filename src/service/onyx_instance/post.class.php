764fae1ea7988914aee98ef286b7e3f628ae0eca
Patrick Emond <emondpd@mcmaster.ca>
2019-01-25 11:29:41 -0500

    Fixing bug when creating onyx-instances
    If the user associated with the instance already exists an error will no
    longer be thrown, instead that existing user will be linked
    appropriately.

--- src/service/onyx_instance/post.class.php
+++ src/service/onyx_instance/post.class.php
@@ -26,27 +26,34 @@ class post extends \cenozo\service\post
    */
   protected function setup()
   {
+    $user_class_name = lib::get_class_name( 'database\user' );
+
     parent::setup();
 
     $db_onyx_instance = $this->get_leaf_record();
     if( is_null( $db_onyx_instance->user_id ) )
-    { // create a user for this onyx instance
+    { // create a user for this onyx instance if it doesn't already exist
       $role_class_name = lib::get_class_name( 'database\role' );
 
       $db_site = lib::create( 'business\session' )->get_site();
       $db_role = $role_class_name::get_unique_record( 'name', 'onyx' );
-      $db_user = lib::create( 'database\user' );
 
       $post_object = $this->get_file_as_object();
-      foreach( $db_user->get_column_names() as $column_name )
-        if( 'id' != $column_name && property_exists( $post_object, $column_name ) )
-          $db_user->$column_name = $post_object->$column_name;
-      $db_user->name = $post_object->username;
-      $db_user->first_name = $db_site->name.' onyx instance';
-      $db_user->last_name = $post_object->username;
-      $db_user->active = true;
-      $db_user->password = util::encrypt( $post_object->password );
-      $db_user->save();
+      $db_user = $user_class_name::get_unique_record( 'name', $post_object->username );
+      if( is_null( $db_user ) )
+      {
+        // no user by that name so make one
+        $db_user = lib::create( 'database\user' );
+        foreach( $db_user->get_column_names() as $column_name )
+          if( 'id' != $column_name && property_exists( $post_object, $column_name ) )
+            $db_user->$column_name = $post_object->$column_name;
+        $db_user->name = $post_object->username;
+        $db_user->first_name = $db_site->name.' onyx instance';
+        $db_user->last_name = $post_object->username;
+        $db_user->active = true;
+        $db_user->password = util::encrypt( $post_object->password );
+        $db_user->save();
+      }
 
       // grant onyx-access to the user
       $db_access = lib::create( 'database\access' );
