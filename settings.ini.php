6690edb5eb885c581f9fb5253ab61ca423a386f2
Patrick Emond <emondpd@mcmaster.ca>
2019-03-18 10:05:44 -0400

    Fixing bug which prevented supporting scripts from being processed

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.4';
-$SETTINGS['general']['build'] = '9821d5f';
+$SETTINGS['general']['build'] = 'a000b67';
 
 // determines whether users other than administrators can see next-of-kin data
 $SETTINGS['general']['next_of_kin'] = false;
