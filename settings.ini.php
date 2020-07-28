615b88938c3d88615b1747e27fe1e31e055897f6
Patrick Emond <emondpd@mcmaster.ca>
2020-07-28 14:54:44 -0400

    Marking start of version 2.6

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.5';
+$SETTINGS['general']['version'] = '2.6';
 $SETTINGS['general']['build'] = '5d38e0c';
 
 // determines whether users other than administrators can see next-of-kin data
