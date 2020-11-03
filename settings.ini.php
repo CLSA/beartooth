613d9b2c2d14b8bcd74b77f031ece4b50ddd266b
Patrick Emond <emondpd@mcmaster.ca>
2020-11-03 09:25:56 -0500

    Updating build number since we seem to be having caching issues

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.6';
-$SETTINGS['general']['build'] = 'aa5ba3a';
+$SETTINGS['general']['build'] = 'b392dad';
 
 // determines whether users other than administrators can see next-of-kin data
 $SETTINGS['general']['next_of_kin'] = false;
