87105d4ec0141aab6195358c9b386e766aadd7cc
Patrick Emond <emondpd@mcmaster.ca>
2020-12-15 16:14:47 -0500

    Giving coordinators access to study and stratum

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.6';
-$SETTINGS['general']['build'] = '58fe6c8';
+$SETTINGS['general']['build'] = '9ffe99a';
 
 // determines whether users other than administrators can see next-of-kin data
 $SETTINGS['general']['next_of_kin'] = false;
