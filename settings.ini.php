4a83be9e1791b1a1f2ea1cc1035f80c029ef1125
Patrick Emond <emondpd@mcmaster.ca>
2021-06-08 10:45:56 -0400

    Changing queue name to title in participant view

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.7';
-$SETTINGS['general']['build'] = '19d34beb';
+$SETTINGS['general']['build'] = 'eefc2a68';
 
 // determines whether users other than administrators can see next-of-kin data
 $SETTINGS['general']['next_of_kin'] = false;
