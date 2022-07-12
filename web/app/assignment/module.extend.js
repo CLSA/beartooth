0dec4f76fa31fe02a0ec2bd5ca665d7bb3612fd5
Patrick Emond <emondpd@mcmaster.ca>
2022-07-12 14:19:37 -0400

    Removing defunct limesurvey code

--- web/app/assignment/module.extend.js
+++ web/app/assignment/module.extend.js
@@ -51,7 +51,6 @@ cenozoApp.extendModule({
         link: function (scope) {
           // update the script list whenever we regain focus since there may have been script activity
           var win = angular.element($window).on("focus", async () => {
-            await scope.model.updateLimesurveyToken();
             scope.model.loadScriptList();
           });
           scope.$on("$destroy", function () {
@@ -609,39 +608,6 @@ cenozoApp.extendModule({
                 await this.loadScriptList();
               } finally {
                 this.scriptLauncherBusy = false;
-              }
-
-              // check for when the window gets focus back and update the participant details
-              if (
-                "pine" != script.application &&
-                null != script.name.match(/withdraw|proxy/i)
-              ) {
-                this.updateLimesurveyTokenScriptId = script.id;
-              }
-            },
-
-            updateLimesurveyTokenScriptId: null,
-            updateLimesurveyToken: async function () {
-              if (null == this.assignment) {
-                this.updateLimesurveyTokenScriptId = null;
-                return;
-              }
-
-              if (this.updateLimesurveyTokenScriptId) {
-                var url =
-                  "script/" +
-                  this.updateLimesurveyTokenScriptId +
-                  "/token/uid=" +
-                  this.participant.uid;
-                this.updateLimesurveyTokenScriptId = null;
-
-                // the following will process the withdraw or proxy script (in case it was finished)
-                try {
-                  this.scriptLauncherBusy = true;
-                  await CnHttpFactory.instance({ path: url }).get();
-                } finally {
-                  this.scriptLauncherBusy = false;
-                }
               }
             },
 
