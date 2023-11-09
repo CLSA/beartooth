c4f4617ad5254302feee364d3d19f7d8994ef5aa
Patrick Emond <emondpd@mcmaster.ca>
2023-11-09 11:24:32 -0500

    Tweak: removing defunct code

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.9';
-$SETTINGS['general']['build'] = '2bf9c987';
+$SETTINGS['general']['build'] = 'af0a456e';
 
 // determines whether users other than administrators can see next-of-kin data
 $SETTINGS['general']['next_of_kin'] = false;
