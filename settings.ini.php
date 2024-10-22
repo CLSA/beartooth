f78daca3d77d9b04890d9bf46c77659368ea59af
Patrick Emond <emondpd@mcmaster.ca>
2024-10-22 14:07:43 -0400

    Bumping to version 2.10

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.9';
+$SETTINGS['general']['version'] = '2.10';
 $SETTINGS['general']['build'] = '579efb6b';
 
 // the location of beartooth internal path
