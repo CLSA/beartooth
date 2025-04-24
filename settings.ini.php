f1c5b1402154fec4257ac48b305b502c1c862e23
Patrick Emond <emondpd@mcmaster.ca>
2025-04-24 15:17:43 -0400

    Bug: couldn't make non-voip calls to international phone numbers

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'beartooth';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.10';
-$SETTINGS['general']['build'] = 'd0e53933';
+$SETTINGS['general']['build'] = 'dfa13231';
 
 // the location of beartooth internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
